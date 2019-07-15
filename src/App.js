803592cd4d84d461786ada2ca7d12e2a3f6ee25a
Pradyumn Nukala <pnukala@unissant.com>
2019-07-15 14:23:36 -0400

    Added Header, with form fields, TimeStamp

--- src/App.js
+++ src/App.js
@@ -5,35 +5,35 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <div class="jumbotron jumbotron-fluid">
+          <div class="container">
+            <h1 class="display-2">AWS Fraud Detection</h1>
+            <p class="lead">A Simple React-based Client that leverages a
+             trained model to predict fraud with AWS | <strong>Pradyumn Nukala</strong></p>
+          </div>
+        </div>
         <form>
           <div class="form-group">
-            <label for="exampleFormControlInput1">Email address</label>
-            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"></input>
+            <label for="exampleFormControlInput1">Transaction Amount</label>
+            <input type="amount" class="form-control" id="exampleFormControlInput1" placeholder="$100"></input>
           </div>
           <div class="form-group">
-            <label for="exampleFormControlSelect1">Example select</label>
-            <select class="form-control" id="exampleFormControlSelect1">
-              <option>1</option>
-              <option>2</option>
-              <option>3</option>
-              <option>4</option>
-              <option>5</option>
+            <label for="exampleFormControlSelect1">Transaction Platform</label>
+            <select class="form-control" exampleFormControlSelect1id="platform">
+              <option>Store</option>
+              <option>Mobile</option>
+              <option>Web</option>
             </select>
           </div>
+
           <div class="form-group">
-            <label for="exampleFormControlSelect2">Example multiple select</label>
-            <select multiple class="form-control" id="exampleFormControlSelect2">
-              <option>1</option>
-              <option>2</option>
-              <option>3</option>
-              <option>4</option>
-              <option>5</option>
-            </select>
+            <label for="exampleFormControlInput1">Date and time</label>
+            <input class="form-control" type="datetime-local" value="2011-08-19T13:45:00" id="example-datetime-local-input"></input>
+
           </div>
           <div class="form-group">
-            <label for="exampleFormControlTextarea1">Example textarea</label>
-            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            <label for="exampleFormControlTextarea1">Raw Data</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="4"></textarea>
           </div>
         </form>
         < button type="button"
